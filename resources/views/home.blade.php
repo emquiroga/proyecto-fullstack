e01042007753c73fa337d39fcb2e0e4cbf9826a2
Emiliano <emiliano.quiroga093@gmail.com>
2021-12-04 14:51:14 -0300

    inicio de la home view, añadí bootstrap para un formulario y los contenedores, por integrar los inputs a la DB

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    {{-- <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
@@ -23,9 +23,42 @@
     <br>
     <hr>
     <br>
-    <br>
+    <br> --}}
     <div class="alert alert-success" role="alert">
         Bienvenidos a nuestro proyecto!
+    </div>
+    <div class="col align-items-start">
+        <div class="col alert alert-danger">
+            <h1>Titulo del proyecto</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, qui animi. Sapiente quia saepe a placeat, quas unde magnam, voluptates dolor delectus, vitae mollitia esse ipsa iste architecto porro soluta!</p>
+            <br>
+            <h2>Integrantes del proyecto</h2>
+            <ul>
+                <li>Integrante</li>
+                <li>Integrante</li>
+                <li>Integrante</li>
+                <li>Integrante</li>
+                <li>Integrante</li>
+            </ul>
+        </div>
+        <div class="col alert alert-success">
+            <h3>Contacto</h3>
+            <form action="">
+                <div class="mb-3">
+                    <label for="nombre" class="form-label">Nombre</label>
+                    <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Ingresa tu nombre completo">
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email" name="email" placeholder="nombre@correo.com">
+                </div>
+                <div class="mb-3">
+                    <label for="mensaje" class="form-label">Tu mensaje</label>
+                    <textarea class="form-control" id="mensaje" name="mensaje" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Enviar</button>
+            </form>
+        </div>
     </div>
 </div>
 @endsection
