719b43867bd4f6254eb9aa0bed557f6de6acc45f
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2021-12-26 15:27:19 -0300

    Categorias: [FIX] Controller, Seeder [quite el icono]

--- resources/views/categorias/categorias.blade.php
+++ resources/views/categorias/categorias.blade.php
@@ -1,15 +1,12 @@
 @extends('layouts.app')
 @section('content')
+
         <div class="container">
 
             @foreach($categorias as $categoria)
-
-
                 <div>
-                    <img  class="categoria_icons icons" src="{{ asset('storage') . '/' . $categoria->icono }}" alt="icon">
                     <a class='link ' href="{{ url('categoria/' . $categoria->id) }}">{{ $categoria->descripcion }}</a>
                 </div>
-
             @endforeach
 
     </div>
