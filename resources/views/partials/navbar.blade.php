050b68134963f6c8d58a8901debb089b52ea9e8c
emquiroga <emiliano.quiroga093@gmail.com>
2021-12-27 11:09:56 -0300

    added search button in navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md shadow-sm">
+<nav class="navbar navbar-expand-lg shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', 'Laravel') }}
@@ -9,6 +9,15 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto">
+                <li>
+                    <form class="form-inline search-form" method="POST" action="{{ url('/libros/busqueda') }} ">
+                    {{csrf_field()}}
+                        <input id="libro" name="libro" class="form-control mr-sm-2 nav-search" type="search" placeholder="Buscar Libro" aria-label="Search">
+                        <button class="icon-btn search-button" type="submit">
+                            <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="svg-inline--fa fa-search fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg>
+                        </button>
+                     </form>
+                </li>
                 <li class="nav-item">
                     <a href={{url('/libros')}} class="nav-link">Libros</a>
                 </li>
@@ -23,13 +32,6 @@
                 </li>
                 <li class="nav-item">
                     <a href={{url('/perfil')}} class="nav-link">Mi Perfil</a>
-                </li>
-                <li>
-                <form class="form-inline" method="POST" action="{{ url('/libros/busqueda') }} ">
-                {{csrf_field()}}
-                    <input id="libro" name="libro" class="form-control mr-sm-2" type="search" placeholder="Buscar Libro" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-                 </form>
                 </li>
                 <li class="nav-item dropdown">
                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -49,7 +51,7 @@
                     </div>
                 </li>
             </ul>
-  
+
         </div>
     </div>
 </nav>
